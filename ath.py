ce60d335f24803560c63111c0c87d797ac5a8b37
Mohammad Moeinish <mohammad.moeinish@gmail.com>
2025-03-31 12:28:58 +0200

    webversion

--- ath.py
+++ ath.py
@@ -1,52 +1,63 @@
-# Write your solution here, DO NOT CREATE A NEW PROJECT
-# WARNING: if you create a new project, your exam paper will not be collected
-#                     and you will be required to attend the next exam session.
-#
-# WARNING: on Windows 10, (Italian keyboard) characters like [ ] { } must be
-#                     created using ALTgr+è (e.g. for [ ) and NOT CTRL-ALT-è
-#
-# WARNING: on macOS, you must use CTRL-C and CTRL-V inside the virtual
-#                     machine and NOT command-C command-V
-#
-# if your keyboard is damaged, you can use the mouse to copy/paste 
-#                     special characters such as [ ] { } < > from this text.
+
 app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
 
 STUDENT_NAME = "Seyedmohammad Moeinishokouh"
 mycloude = "gitttttttt"
-print("Exam of " + STUDENT_NAME)
-
-# Write here your solution:
 
 def finalscore(athletic):
     score = []
     for i in range(4,9):
         score.append(float(athletic[i]))
     f_score = (sum(score) - (max(score)+min(score)))/3
-    return(f_score)
+    return f_score
 
-
-def main():
+def calculate_results():
     with open('athletes_score.txt','r') as athletes:
         max_score = 0
         country_score = {}
         country_list = []
+
         for athletic in athletes.readlines():
             athletic = athletic.strip().split()
             score = finalscore(athletic)
+
             if score > max_score and athletic[2] == 'F':
                 winner = athletic
                 max_score = score
-            if athletic[3] in country_score :
-                country_score[athletic[3]] = country_score[athletic[3]] + score
-            else :
-                country_score[athletic[3]] = score
-        print("Women's winner:\n%s %s,%s - score: %.1f\n" % (winner[0],winner[1],winner[3],max_score))
-        for country , C_score in country_score.items():
-            country_list.append([C_score,country])
-        country_list.sort(reverse=True)
-        print('Overall national ranking:')
-        for j in range(3):
-            print('%d) %s - Total Score : %.1f'%((j+1),country_list[j][1],country_list[j][0]))
 
-main()
+
+def main():
+
+            if athletic[3] in country_score:
+                country_score[athletic[3]] += score
+            else:
+                country_score[athletic[3]] = score
+
+            winner_result = f"{winner[0]} {winner[1]}, {winner[3]} - score: {max_score:.1f}"
+        
+            for country, C_score in country_score.items():
+                country_list.append([C_score, country])
+                country_list.sort(reverse=True)
+
+            ranking = []
+            for j in range(min(3, len(country_list))):
+             ranking.append(f"{j+1}) {country_list[j][1]} - Total Score : {country_list[j][0]:.1f}")
+
+            return winner_result, ranking
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/run')
+def run_script():
+    winner, ranking = calculate_results()
+    return render_template('result.html', winner=winner, ranking=ranking)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
